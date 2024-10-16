e4e237cdd06046d1656883e3732863da5f3214c9
YutaMatsumoto <k024g0066@g.neec.ac.jp>
2024-10-16 12:40:49 +0900

    衝突テスト

--- PM1_1_HelloGit.cpp
+++ PM1_1_HelloGit.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello Git!\n";
-    std::cout << "PullReqest Branch!\n";
+    std::cout << "Conflict changed!\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
